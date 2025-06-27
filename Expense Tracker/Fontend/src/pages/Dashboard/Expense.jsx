c360908baf75b703e4db4721c65bc27090068e83
ashutosh <kurvheashutosh@gmail.com>
2025-06-27 23:53:03 +0530

    Making some changes

--- Expense Tracker/Fontend/src/pages/Dashboard/Expense.jsx
+++ Expense Tracker/Fontend/src/pages/Dashboard/Expense.jsx
@@ -108,7 +108,7 @@ const Expense = () => {
       );
 
       //Create a URL for the blob
-      const url = window.URL.createObjectURL(new Blob([response.date]));
+      const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", "expense_details.xlsx");
